4a86f0772e472a33e882d84e604cdee2272a8944
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-01-02 21:32:05 +0200

    add solution for the pazzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,75 @@
 import './App.css';
+import { allData } from './services/firstData';
+import './App.css';
 
 function App() {
+  const allData3 = [];
+
+  for (let data = 0; data < allData.length; data += 6) {
+    const number =
+      allData[data] +
+      allData[data + 1] +
+      allData[data + 2] +
+      allData[data + 3] +
+      allData[data + 4] +
+      allData[data + 5];
+    allData3.push(number);
+  }
+
+  console.log('allData', allData3);
+
+  const allMatches = [];
+  let match = '';
+
+  for (let number = 0; number < allData3.length; number++) {
+    const first = allData3[number];
+
+    let lastPart = first.slice(4);
+    let firstPart = first.slice(0, 5);
+    // console.log(lastPart);
+
+    match = firstPart;
+
+    for (let j = 0; j < allData3.length; j++) {
+      if (allData3[j] === allData3[number]) {
+        continue;
+      }
+
+      if (allData3[j].startsWith(lastPart)) {
+        match += firstPart;
+
+        lastPart = allData3[j].slice(4);
+        firstPart = allData3[j].slice(0, 5);
+      }
+    }
+
+    allMatches.push(match);
+  }
+  console.log(allMatches);
+
+  const newNumberResult = allMatches.map((match) => +match);
+  console.log(newNumberResult);
+
   return (
     <>
-      <h2>Hello World</h2>
-      <h3>dfsd</h3>
+      <h2>Pazzle</h2>
+
+      <div className="result">
+        <div className="all-data">
+          {allData3.map((data) => {
+            return (
+              <p className="data" key={data}>
+                {data}
+              </p>
+            );
+          })}
+        </div>
+
+        <div className='result__end'>
+          <span>Результат </span>
+          <span>{Math.max(...newNumberResult)}</span>
+        </div>
+      </div>
     </>
   );
 }
